866a97ecd5224bcc59f62698a3eef12f581dbc47
Hristo Dimitrov <dimitrov_hristo@mail.bg>
2022-04-06 10:20:59 +0300

    keep coding

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,10 +9,10 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
-        <img src={mealsImage} alt="A table full of delicious food!"/>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
